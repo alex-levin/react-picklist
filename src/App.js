27d89b43d4a295ceac63fc130c7ce34d9916dc6f
alex-levin <alexlevinb@gmail.com>
2019-02-16 19:56:13 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
-import React, { Component } from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import Icon from '@material-ui/core/Icon';
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Icon from "@material-ui/core/Icon";
 
 const styles = theme => ({
   root: {
-    width: '50%',
+    width: "50%",
     maxWidth: 260,
-    backgroundColor: theme.palette.background.paper,
+    backgroundColor: theme.palette.background.paper
   },
+  paper: {
+    height: 200
+  }
 });
 
 class App extends Component {
   state = {
-    selectedIndex: 0,
+    leftList: ["Honda", "Toyota", "Mazda"],
+    rightList: ["Subaru"]
   };
 
-  handleListItemClick = (event, index) => {
-    console.log('index:' + index);
+  handleLeftListItemClick = (event, index) => {
+    const items = this.state.leftList;
+    // remove selected items
+    this.setState({ leftList: [...items.slice(0, index), ...items.slice(index + 1)] });
+  };
+
+  handleRightListItemClick = (event, index) => {
+    console.log("index:" + index);
     this.setState({ selectedIndex: index });
 
     console.log(this.state);
@@ -29,30 +39,33 @@ class App extends Component {
 
   render() {
     const { classes } = this.props;
+    const { leftList } = this.state;
+    const { rightList } = this.state;
+
+    const leftListItems = leftList.map((item, index) => <ListItem key={item}
+      button
+      selected={this.state.selectedIndex === 0}
+      onClick={event =>
+        this.handleLeftListItemClick(event, index)
+      }
+    >
+      <ListItemText primary={item} />
+    </ListItem>);
+
+    leftList.forEach((item, index) => console.log(item, index));
 
     return (
       <div className={classes.root}>
         <Grid container>
           <Grid item xs>
-            <Paper>
-              <List component="nav">
-                <ListItem button selected={this.state.selectedIndex === 0}
-                  onClick={event => this.handleListItemClick(event, 0)}>
-                  <ListItemText primary="Honda" />
-                </ListItem>
-                <ListItem button
-                  onClick={event => this.handleListItemClick(event, 1)}>
-                  <ListItemText primary="Toyota" />
-                </ListItem>
-                <ListItem button
-                  onClick={event => this.handleListItemClick(event, 2)}>
-                  <ListItemText primary="Mazda" />
-                </ListItem>                        
+            <Paper className={classes.paper}>
+              <List component="nav" dense={true}>
+                  {leftListItems}
               </List>
-            </Paper>          
+            </Paper>
           </Grid>
           <Grid item xs>
-            <List component="nav">
+            <List component="nav" dense={true}>
               <ListItem button>
                 <Icon>keyboard_arrow_right</Icon>
               </ListItem>
@@ -62,16 +75,16 @@ class App extends Component {
             </List>
           </Grid>
           <Grid item xs>
-            <Paper>
-              <List component="nav">
+            <Paper className={classes.paper}>
+              <List component="nav" dense={true}>
                 <ListItem button>
                   <ListItemText primary="Subaru" />
-                </ListItem>               
+                </ListItem>
               </List>
-            </Paper>              
-          </Grid>          
+            </Paper>
+          </Grid>
         </Grid>
-        Selected {this.state.selectedIndex}   
+        Selected {this.state.selectedIndex}
       </div>
     );
   }
